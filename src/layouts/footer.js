eae88bbee015461d5e60e949001cd92f362201a2
Stephen Yang <stepheny755@gmail.com>
2023-07-02 20:46:36 -0400

    updated website stuff

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import SimpleReactFooter from 'simple-react-footer';
 
 class Footer extends React.Component {
     render() {
         return (
             <footer>
-              <a href="https://github.com/Stepheny755" ><i className="fab fa-github"></i></a>
-              <a href="https://www.instagram.com/stephh.yang/" ><i className="fab fa-instagram"></i></a>
-              <a href="https://www.facebook.com/stephy755" ><i className="fab fa-facebook-f"></i></a>
-              <a href="https://www.linkedin.com/in/stepheny755/" ><i className="fab fa-linkedin-in"></i></a>
-              <a href="https://twitter.com/stepheny755" ><i className="fab fa-twitter"></i></a>
+                <div>
+                    <a href="https://github.com/Stepheny755" ><i className="fab fa-github"></i></a>
+                    <a href="https://www.instagram.com/stephh.yang/" ><i className="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/stephy755" ><i className="fab fa-facebook-f"></i></a>
+                    <a href="https://www.linkedin.com/in/stepheny755/" ><i className="fab fa-linkedin-in"></i></a>
+                    <a href="https://twitter.com/stepheny755" ><i className="fab fa-twitter"></i></a>
+                </div>
             </footer>
         );
     }
