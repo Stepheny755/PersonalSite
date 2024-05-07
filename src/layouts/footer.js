43e5abb1f8895a309223499d661ff686efde058f
Stephen Yang <stepheny755@gmail.com>
2024-05-06 19:55:00 -0700

    update footer

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -7,9 +7,9 @@ class Footer extends React.Component {
             <footer>
                 <div>
                     <h4>
+                    <a href="resume.pdf" className="footer_elem">resume</a>
+                    <a href="mailto:stephy.yang@mail.utoronto.ca" className="footer_elem">email</a>
                     <a href="https://github.com/Stepheny755" className="footer_elem">github</a>
-                    <a href="https://www.instagram.com/stephh.yang/" className="footer_elem">insta</a>
-                    <a href="https://www.facebook.com/stephy755" className="footer_elem">facebook</a>
                     <a href="https://www.linkedin.com/in/stepheny755/" className="footer_elem">linkedin</a>
                     <a href="https://neal.fun/password-game/" className="footer_elem">fun</a>
                     </h4>
