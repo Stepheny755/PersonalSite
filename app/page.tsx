7c0d408e2672b2169846e2675322224c09e392f2
Stephen Yang <stephy.yang@mail.utoronto.ca>
2024-08-11 17:33:45 -0400

    add more links

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
               My name is Stephen Yang, and welcome to my corner of the internet!
             </p>
             <p className="my-4">
-              I&apos;m interested in mechatronics and the intersection of software engineering and physical systems. I studied computer engineering at the University of Toronto. Currently, I&apos;m working through my Masters of Applied Science under the supervision of professor Eric Diller, where I focus on the simulation and development of millimeter scale medical microrobots.
+              I&apos;m interested in mechatronics and the intersection of software engineering and physical systems. I studied computer engineering at the <a href="https://www.utoronto.ca/" className="hblink">University of Toronto</a>. Currently, I&apos;m working through my MASc under the supervision of professor <a href="https://www.mie.utoronto.ca/faculty_staff/diller/" className="hblink">Eric Diller</a>, where I focus on the simulation and development of millimeter scale medical microrobots.
             </p>
             <p className="my-4">
               Previously, I worked on FPGA IP Development at <a href="https://www.intel.com/content/www/us/en/products/programmable.html" className="hblink">Intel</a>, and at the <a href="https://www.dsp.utoronto.ca/" className="hblink">UofT Multimedia Lab</a> on ML for computational pathology. Read more on my <a href="/docs/resume.pdf" className="hblink">resume</a>.
