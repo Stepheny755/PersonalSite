422d493b8f618eec6fb42a7cc13c22ce76d48bbc
Stephen Yang <stephy.yang@mail.utoronto.ca>
2024-11-26 01:55:58 -0500

    [links] add capstone and CV docs

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
               I&apos;m interested in mechatronics and the intersection of software engineering and physical systems. I studied computer engineering at the <a href="https://www.utoronto.ca/" className="hblink">University of Toronto</a>. Currently, I&apos;m working through my MASc under the supervision of professor <a href="https://www.mie.utoronto.ca/faculty_staff/diller/" className="hblink">Eric Diller</a>, where I focus on the simulation and development of millimeter scale medical microrobots.
             </p>
             <p className="my-4">
-              Previously, I worked on FPGA IP Development at <a href="https://www.intel.com/content/www/us/en/products/programmable.html" className="hblink">Intel</a>, and at the <a href="https://www.dsp.utoronto.ca/" className="hblink">UofT Multimedia Lab</a> on ML for computational pathology. Read more on my <a href="/docs/resume.pdf" className="hblink">resume</a>.
+              Previously, I worked on FPGA IP Development at <a href="https://www.intel.com/content/www/us/en/products/programmable.html" className="hblink">Intel</a>, and at the <a href="https://www.dsp.utoronto.ca/" className="hblink">UofT Multimedia Lab</a> on ML for computational pathology. Read more on my <a href="/docs/resume.pdf" className="hblink">resume</a> or <a href="/docs/CV.pdf" className="hblink">CV</a>.
             </p>
             <p className="my-4">
               In my free time, I enjoy brewing coffee (V60 and Moka), lifting rocks, 3D printing, taking <a href="https://www.instagram.com/stephyangshoots" className="hblink">photos</a> and <a href="https://app.thestorygraph.com/profile/stepheny" className="hblink">reading</a>.
@@ -30,6 +30,9 @@ export default function Home() {
               </li>
               <li className="text-stone-800 dark:text-stone-100">
                 <a href="/docs/ds_in_epilepsy.pdf" className="hlink">Dynamical Systems for Epilepsy Prediction</a>
+              </li>
+              <li className="text-stone-800 dark:text-stone-100">
+                <a href="/docs/capstone.pdf" className="hlink">Actuator Modelling for Simulation of Energy-Efficient Human-Robot Walking</a>
               </li>
             </ul>
             <br></br>
