d9d28d903ab70330379474a62dedf239cb98a4fa
Stephen Yang <stephy.yang@mail.utoronto.ca>
2024-11-26 22:10:12 -0500

    [404] formatting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,18 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="bg-l_body dark:bg-d_body">
+      <div className="mx-auto w-full max-w-screen-xl px-2 lg:px-22 md:px-16 sm:px-10">
+        <div className="container text-stone-800 dark:text-stone-100">
+          <h1 className="my-6 mt-20 text-4xl">Not Found</h1>
+          <p>
+            Could not find requested resource
+          </p>
+          <p>
+            Click to return <Link href="/" className="hlink">home</Link>.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
