d6f3eca28e9f4d6bd24aa7ba7a4d75ccd70678ca
Stephen Yang <stephy.yang@mail.utoronto.ca>
2025-01-08 14:25:49 -0500

    [webapp] rename

---
+++ app/(interactives)/gestaltung/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Gestaltung() {
+  return (
+    <div className="bg-l_body dark:bg-d_body">
+      <div className="mx-auto w-full max-w-screen-xl px-2 lg:px-22 md:px-16 sm:px-10">
+        <div className="container text-stone-800 dark:text-stone-100">
+          <p>hihi</p>
+        </div>
+      </div>
+    </div >
+  );
+}
